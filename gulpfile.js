29e2230d04616febcbb1b4aac50dc9fae2fa66bf
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2017-12-12 21:03:40 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,13 @@ var gulp = require('gulp'),
     gp_rename = require('gulp-rename'),
     gp_uglify = require('gulp-uglify');
 
-var dgFileModuleSrc = ['./src/*.js'];
+var dgFileModuleSrc = [
+  './src/*.js',
+  './src/classes/class.*.js',
+  './src/includes/include.*.js',
+  './src/forms/form.*.js',
+  './src/widgets/widget.*.js'
+];
 
 // Task to build the dg_file.min.js file.
 gulp.task('minifyJS', function(){
