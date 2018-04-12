7425d631ba83c8d90f7064f009094c8771a987fc
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-04-12 14:36:46 -0400

    Change theme_file() to set aside the widget and immediately render the corresponing theme_file_widget().

--- src/widgets/widget.file.js
+++ src/widgets/widget.file.js
@@ -1,39 +1,28 @@
+/**
+ *
+ * @param variables
+ *    _filePathDir {String} the folder in the file system to save the file in, e.g. "articles/images"
+ * @returns {string}
+ */
 dg.theme_file = function(variables) {
-  console.log('theme_file', variables);
+  //console.log('theme_file', variables);
   var attrs = variables._attributes;
 
-  if (!attrs.id) { attrs.id = 'file-' + jDrupal.userPassword(); }
+  // Make sure we have an id for the widget.
+  if (!attrs.id) { attrs.id = 'file-' + dg.salt(); }
+  var id = attrs.id;
+
+  // Set aside the widget so it can be used later.
+  dg_file.setFileWidget(id, variables);
+
+  // We force a hidden input as the place we track the file id.
   attrs.type = 'hidden';
 
-  var fileWrapperAttrs = {
-    class: 'dg-file-wrapper',
-    'data-id': attrs.id
-  };
-
+  // Return the hidden input followed by the file input widget.
   return '<input ' + dg.attrs(variables) + ' />' +
-      '<div ' + dg.attributes(fileWrapperAttrs) + '>' + dg.b(dg.t('Attach file'), {
-    _attributes: {
-      onclick: 'dg_file.attachFileOnclick(this)',
-      title: variables._title ? variables._title : dg.t('File'),
-      'data-form-input-id': attrs.id
-    }
-  }) + '</div>';
-};
-
-/**
- * Handles clicks on the "Attach file" button by loading the FileForm into a modal window.
- * @param button
- * @returns {boolean}
- */
-dg_file.attachFileOnclick = function(button) {
-  var element = {};
-  element.controls = {
-    _theme: 'form',
-    _id: 'FileForm',
-    _formInputId: button.getAttribute('data-form-input-id')
-  };
-  dg.modal(element, {
-    title: button.getAttribute('title')
-  });
-  return false;
+          dg.render({
+            _prefix: '<div id="dg-file-form-messages"></div>',
+            _theme: 'file_input',
+            _formInputId: id
+          });
 };
