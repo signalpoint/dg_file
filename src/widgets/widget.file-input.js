d9449a9d761cf17a70101fdf70be9bbd813a881c
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-04-13 14:11:06 -0400

    Completely simply theme_file_input() by abstracting out DgFileInput.html().

--- src/widgets/widget.file-input.js
+++ src/widgets/widget.file-input.js
@@ -5,99 +5,23 @@
  */
 dg.theme_file_input = function(variables) {
   //console.log('theme_file_input', variables);
-  var attrs = variables._attributes;
 
-  // Generate an attribute id if one wasn't provided.
-  if (!attrs.id) { attrs.id = 'dg-file-widget-' + dg.salt(); }
-  var id = attrs.id;
+  // Create a new file input, set it aside for use down the line and then return its html.
+  var fileInput = new DgFileInput(variables);
+  dg_file.setFileInput(fileInput);
+  return fileInput.html();
 
-  // Set this file input's variables aside for re-use.
-  dg_file.setFileInput(id, variables);
-
-  // Generate a wrapper id.
-  var wrapperId = id + '-wrapper';
-
-  var html = '<div id="' + wrapperId + '" class="dg-file-widget-wrapper">';
-
-  if (variables._file) { // @TODO implement.
-
-    // There is an existing file...
-
-    var file = variables._file;
-    //console.log('existing file', file);
-    //var preview = theme('image_style', {
-    //  style_name: 'thumbnail',
-    //  path: file.uri
-    //});
-    //var deleteBtn = bl(t('Remove'), null, {
-    //  attributes: {
-    //    'data-icon': 'delete',
-    //    'data-iconpos': 'notext',
-    //    onclick: 'dgFileWidgetRemoveOnclick(' + file.fid + ')',
-    //    wrapperId: wrapperId
-    //  }
-    //});
-    //html += deleteBtn + preview;
-
-  }
-  else {
-
-    // There is no default file...
-
-    // Generate a preview identifier.
-    var previewId = id + '-preview';
-    var formInputId = variables._formInputId;
-
-    // File input widget.
-    var input = null;
-
-    // Compiled widget.
-    if (dg.isCompiled()) {
-
-      attrs.onclick = "dg_file.openFilePicker(this, '" + previewId + "', '" + formInputId + "')";
-      input = dg.b(dg.t('Choose File'), { _attributes: attrs });
-
-    }
-    else { // web app widget.
-
-      if (!attrs.type) { attrs.type = 'file'; }
-      if (!attrs.onchange) {
-        attrs.onchange = "dg_file.chooseFileOnchange(" +
-            "'" + wrapperId + "', " +
-            "'" + attrs.id + "', " +
-            "'" + previewId + "', " +
-            "'" + formInputId + "'" +
-            ")";
-      }
-      attrs.class.push('dg-file-widget');
-      input = '<input ' +  dg.attrs(variables) + '/>';
-    }
-
-    // Preview widget.
-    var previewAttrs = {
-      id: previewId,
-      src: '',
-      class: ['dg-file-preview']
-    };
-    var preview = '<img ' + dg.attributes(previewAttrs) + '>';
-
-    // Add input and empty preview to html.
-    html += input + preview;
-
-  }
-
-  return html + '</div>';
-  
 };
 
-dg_file.chooseFileOnchange = function(wrapperId, inputId, previewId, formInputId) { // web-app only...
+//dg_file.chooseFileOnchange = function(wrapperId, inputId, previewId, formInputId) { // web-app only...
+dg_file.chooseFileOnchange = function(fileInputId) { // web-app only...
   console.log('chooseFileOnchange', arguments);
 
   // Load up the file input's variables.
-  var variables = dg_file.getFileInput(inputId);
-  console.log('variables', variables);
+  var fileInput = dg_file.getFileInput(fileInputId);
+  console.log('fileInput', fileInput);
 
-  var input = dg.qs('#' + inputId);
+  var input = fileInput.getInput();
   console.log('input', input);
 
   // Grab the file from the file input element.
@@ -105,21 +29,6 @@ dg_file.chooseFileOnchange = function(wrapperId, inputId, previewId, formInputId
   var file = input.files[0];
   if (!file) { return; }
   console.log('file input', file);
-
-  // If they previously selected a file for preview, then delete it from Drupal's file management since it will be
-  // unused.
-  //var fid = $('#' + inputId).attr('fid');
-  //if (fid) {
-  //  console.log('fid', fid);
-  //  file_delete(fid, {
-  //    success: function(result) {
-  //      dg_file.removeFromPendingFileIds(fid);
-  //      dg_file.read();
-  //    },
-  //    error: dg.error
-  //  });
-  //}
-  //else { dg_file.read(); }
 
   // Init a file reader.
   var reader = new FileReader();
@@ -129,7 +38,8 @@ dg_file.chooseFileOnchange = function(wrapperId, inputId, previewId, formInputId
 
     // The file has been loaded...
 
-    dg_file.loaded(file, reader.result, inputId, previewId, formInputId);
+    //dg_file.loaded(file, reader.result, inputId, previewId, formInputId);
+    dg_file.loaded(file, reader.result, fileInputId);
 
   }, false);
 
