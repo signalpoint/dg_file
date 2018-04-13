d8cfa4f1acb406c4d75d4e951fb7ae0c57125a3c
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-04-13 14:07:04 -0400

    Update setFileInput() to work with DgFileInput prototype.

--- src/_dg_file.js
+++ src/_dg_file.js
@@ -1,19 +1,25 @@
 dg.createModule('dg_file');
 
 // @see widget.file.js
+
 dg_file._fileWidgets = {};
+
 dg_file.getFileWidget = function(id) {
   return dg_file._fileWidgets[id] ? dg_file._fileWidgets[id] : null;
 };
+
 dg_file.setFileWidget = function(id, variables) {
   dg_file._fileWidgets[id] = variables;
 };
 
 // @see widget.file-input.js
+
 dg_file._fileInputs = {};
+
 dg_file.getFileInput = function(id) {
   return dg_file._fileInputs[id] ? dg_file._fileInputs[id] : null;
 };
-dg_file.setFileInput = function(id, variables) {
-  dg_file._fileInputs[id] = variables;
+
+dg_file.setFileInput = function(fileInput) {
+  dg_file._fileInputs[fileInput.id()] = fileInput;
 };
